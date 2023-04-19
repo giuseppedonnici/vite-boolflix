0259d105712aa174511893a52e08689dca706f77
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-19 15:55:45 +0200

    Add: AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<script>
+    import {store} from "../store";
+
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+                store
+            }
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="logo">BOOLFLIX</div>
+            <div class="searchbar">
+                <input class="w-50" type="text" placeholder="Inserisci il film da cercare..." v-model="store.search">
+                <button class="ms-4">Cerca</button>
+            </div>
+        </div>
+    </header>
+
+</template>
+
+<style scoped lang="scss">
+    header {
+        height: 100px;
+        background-color: black;
+        .container {
+            height: 100%;
+            .logo {
+                font-size: 2rem;
+                color: red;
+            }
+            .searchbar {
+                text-align: end;
+                width: 60%;
+            }
+        }
+    }
+</style>
