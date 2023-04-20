886d118aae9f6070eabbc9558cc81df274cda273
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-04-20 15:03:23 +0200

    Add: getMovies call to App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
-import {store} from "./store";
+import { store } from "./store";
 import axios from "axios";
 
 export default {
@@ -16,35 +16,46 @@ export default {
   },
   methods: {
     handleSearch() {
-      if(this.store.query) {
+      if (this.store.query) {
         this.getMovies();
+        this.getSeries();
       }
     },
     getMovies() {
       axios.get(`${this.store.apiURL}/search/movie`, {
-          params: {
-            api_key: this.store.api_key,
-            query: this.store.query
-          }
-        }).then(resp => {
-          this.store.movies = resp.data.results;
-        }).catch(error => {
-          console.log(error);
-        })
+        params: {
+          api_key: this.store.api_key,
+          query: this.store.query
+        }
+      }).then(resp => {
+        this.store.movies = resp.data.results;
+      }).catch(error => {
+        console.log(error);
+      })
+    },
+    getSeries() {
+      axios.get(`${this.store.apiURL}/search/tv`, {
+        params: {
+          api_key: this.store.api_key,
+          query: this.store.query
+        }
+      }).then(resp => {
+        this.store.series = resp.data.results;
+      }).catch(error => {
+        console.log(error);
+      })
     }
   }
 }
 </script>
 
 <template>
-  <AppHeader @search="handleSearch"/>
+  <AppHeader @search="handleSearch" />
   <div class="container">
     <AppMain />
-    
   </div>
 </template>
 
 <style lang="scss">
-  @use "./styles/general.scss";
-
+@use "./styles/general.scss";
 </style>
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { reactive } from "vue";
 
 export const store = reactive({
     movies: [],
+    series: [],
     apiURL: "https://api.themoviedb.org/3",
     api_key: "a75b5c85872faec5e03dcc299638b8a5",
     query: ""
